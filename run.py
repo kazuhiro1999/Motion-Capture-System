0188a2e7852b4618ffff375a2a26be4d84ebb355
esaki <machinedoll.kazu@gmail.com>
2023-09-16 03:25:28 +0900

    データをキューに追加するように変更

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import argparse
 import os
+import cv2
+import numpy as np
 from controller import MotionCaptureController
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', default="config1.json")
-    parser.add_argument('--host', default="127.0.0.1")
-    parser.add_argument('--port', default=50000)
+    parser.add_argument('--config', default="config.json")
 
     args = parser.parse_args()
     return args
@@ -19,12 +19,18 @@ def main():
     config_path = os.path.join(script_dir, args.config)
 
     controller = MotionCaptureController()
-    controller.initialize(config_path=config_path, udp_host=args.host, udp_port=args.port)
+    controller.initialize(config_path=config_path)
     
     #controller.debug = True
     controller.start_capture(mode='multi-process')
     
-    input()
+    while True:
+        data = controller.get_data()
+        if data:
+            cv2.imshow(data['TimeStamp'], np.zeros([64,64]))
+        if cv2.waitKey(1) == 27:
+            break
+
     controller.end_capture()
 
 
